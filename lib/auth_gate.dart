0932ed93326455a2885bf0f5f55ac123c1bc6886
shimayu616 <shimayu616@icloud.com>
2025-06-19 14:10:20 -0700

    file organize and account auth setting

---
+++ lib/auth_gate.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<fb_auth.User?>(
+      stream: fb_auth.FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+              GoogleProvider(clientId: "YOUR_WEBCLIENT_ID"),
+              AppleProvider(), // Appleログインを使う場合
+            ],
+          );
+        }
+        return const TimerSetListPage();
+      },
+    );
+  }
+}
